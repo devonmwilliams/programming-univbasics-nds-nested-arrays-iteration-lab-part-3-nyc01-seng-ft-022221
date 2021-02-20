79a6bd9d163886961310308841f06d3280de366a
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-20 09:59:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,6 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
       new_string += src[row_index][element_index]
-    end
     element_index += 1
   end
   row_index += 1
